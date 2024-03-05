ed42596cd26e9a550b2a81e3e0b680feb460a78c
Lazy Cat <mylazysundaycat@gmail.com>
2024-03-05 16:34:22 +0900

    인가없이 해당 경로에 접근 가능한 configuration

---
+++ src/main/java/me/silvernine/jwttutorial/config/SecurityConfig.java
@@ -0,0 +1,27 @@
+package me.silvernine.jwttutorial.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.SecurityBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((authorize) -> authorize
+                        .requestMatchers("/api/hello").permitAll()
+                        .anyRequest().authenticated()
+                );
+
+        return http.build();
+    }
+}
